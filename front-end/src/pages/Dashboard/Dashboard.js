46c36ded804c19d4b6a1b1a2f975d6f2900b61aa
hamzaguyo725 <hamzaguyo725@gmail.com>
2023-06-09 13:01:39 +0300

    update

--- front-end/src/pages/Dashboard/Dashboard.js
+++ front-end/src/pages/Dashboard/Dashboard.js
@@ -38,7 +38,7 @@ const Dashboard = () => {
             <svg
               xmlns="http://www.w3.org/2000/svg"
               viewBox="0 0 24 24"
-              id="bookmark"
+              
               
               
               
