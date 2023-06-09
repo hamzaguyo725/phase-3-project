83a742e38ad49eaec7e142d13b94164a50eb6c86
hamzaguyo725 <hamzaguyo725@gmail.com>
2023-06-09 13:00:36 +0300

    update

--- front-end/src/pages/Dashboard/Dashboard.js
+++ front-end/src/pages/Dashboard/Dashboard.js
@@ -40,7 +40,7 @@ const Dashboard = () => {
               viewBox="0 0 24 24"
               id="bookmark"
               height="56"
-              width="56"
+              
               
             >
               <g data-name="Layer 2">
